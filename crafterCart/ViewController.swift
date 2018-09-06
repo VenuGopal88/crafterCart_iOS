8613a063d528968e8e5ef906f5289009bea744b4
Venu Gopal Gupta <VenuGopal.Gupta@honeywell.com>
2018-09-06 10:03:08 +0530

    update

--- crafterCart/ViewController.swift
+++ crafterCart/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import WebKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var webView: WKWebView!
+	public let webURL:String? = nil
+	let reachabilityManager = NetworkReachabilityManager()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		reachabilityManager?.startListening()
+		
+		let url = URL (string: "http://store.craftercart.com")
+		let request = URLRequest.init(url: url!)
+		webView.load(request)
 	}
 
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		reachabilityManager?.stopListening()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		reachabilityManager?.listener = { _ in
+			if let isNetworkReachable = self.reachabilityManager?.isReachable,
+				isNetworkReachable == true {
+				//Internet Available
+			} else {
+				//Internet Not Available"
+			}
+		}
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
 }
 
